cfc6f459ae8876075bf6c1e8ee703d857149a750
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2021-11-12 13:56:35 -0300

    Validação no Método update()

--- src/main/java/br/com/etaure/resource/PassagemResource.java
+++ src/main/java/br/com/etaure/resource/PassagemResource.java
@@ -27,49 +27,49 @@ import br.com.etaure.entities.Passagem;
 
 @Path("passagens")
 public class PassagemResource {
-	
+
 	private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 	private Validator validator = factory.getValidator();
-	
+
 	// Lista todas as Passagens
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response listAll() {
 		List<Passagem> passagens = PassagemDAO.findAll();
-		
-		if(passagens.isEmpty()) {		
+
+		if (passagens.isEmpty()) {
 			return Response.status(Status.NO_CONTENT).build();
 		} else {
 			// Caso tenha ao menos 1 passagem, retorna todas as passagens registradas
 			StringBuilder sb = new StringBuilder();
-			
+
 			for (Passagem passagem : passagens) {
 				sb.append(passagem.toJson());
 			}
-			
+
 			return Response.ok(sb.toString()).build();
 		}
 	}
-	
+
 	// Lista a Passagem com o id passado por parâmetro
 	@GET
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response findById(@PathParam("id") Integer id) {
 		Passagem passagem = PassagemDAO.findById(id);
-		
-		if(passagem == null) {
+
+		if (passagem == null) {
 			return Response.status(Status.NOT_FOUND).build();
 		} else {
 			return Response.ok(passagem.toJson()).build();
 		}
 	}
-	
+
 	// Insere uma nova Passagem para o banco de dados
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response add(String passagemJson) throws URISyntaxException {
-		// TODO Melhoria pra receber o Json sem o "id"		
+		// TODO Melhoria pra receber o Json sem o "id"
 		Passagem passagem = new Gson().fromJson(passagemJson, Passagem.class);
 
 		passagem.setId(null);
@@ -77,54 +77,67 @@ public class PassagemResource {
 		// Verifica se os campos são válidos
 		Set<ConstraintViolation<Passagem>> violations = validator.validate(passagem);
 
-		if(violations.isEmpty()) {
+		if (violations.isEmpty()) {
 			PassagemDAO.insert(passagem);
-			
+
 			// Prepara a URI para redirecionar o cliente para a nova passagem
 			URI uri = new URI("passagens/" + passagem.getId());
-			
+
 			return Response.created(uri).status(Status.CREATED).build();
 		} else {
 			for (ConstraintViolation<Passagem> violation : violations) {
 				System.out.println(violation.getMessage());
 			}
-			
+
 			return Response.status(Status.NOT_ACCEPTABLE).build();
 		}
 	}
-	
-	// Atualiza uma Passagem que já existe no banco de acordo com o "id" passado como parâmetro pela requisição HTTP
+
+	// Atualiza uma Passagem que já existe no banco de acordo com o "id" passado
+	// como parâmetro pela requisição HTTP
 	@PUT
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response update(@PathParam("id") Integer id ,String passagemJson) throws URISyntaxException {
+	public Response update(@PathParam("id") Integer id, String passagemJson) throws URISyntaxException {
 		Passagem newPassagem = new Gson().fromJson(passagemJson, Passagem.class);
 		
 		Passagem oldPassagem = PassagemDAO.findById(id);
 		
-		if(oldPassagem == null) {
+		if (oldPassagem == null) {
 			return Response.status(Status.NOT_FOUND).build();
 		} else {
-			PassagemDAO.updatePassagem(id, newPassagem);
-			
-			return Response.ok().build();
+			// Verifica se os campos são válidos
+			Set<ConstraintViolation<Passagem>> violations = validator.validate(newPassagem);
+
+			if(violations.isEmpty()) {
+				PassagemDAO.updatePassagem(id, newPassagem);
+
+				return Response.ok().build();
+			} else {
+				for (ConstraintViolation<Passagem> violation : violations) {
+					System.out.println(violation.getMessage());
+				}
+
+				return Response.status(Status.NOT_ACCEPTABLE).build();
+			}
+		
 		}
 	}
-	
+
 	// Deleta uma passagem do banco
 	@DELETE
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response delete(@PathParam("id") Integer id) {
 		Passagem passagem = PassagemDAO.findById(id);
-		
-		if(passagem == null) {
+
+		if (passagem == null) {
 			return Response.status(Status.NOT_FOUND).build();
 		} else {
 			PassagemDAO.deletePassagem(id);
-			
+
 			return Response.ok().build();
 		}
 	}
-	
+
 }
